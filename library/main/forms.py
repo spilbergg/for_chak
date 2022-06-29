7860d41be3427a4839127caed0142dc2a31ad7b0
unknown <egor.kushner.00@mail.ru>
2022-06-29 12:28:39 +0300

    readers

--- library/main/forms.py
+++ library/main/forms.py
@@ -1,6 +1,11 @@
 from django import forms
+from django.forms import TextInput, Select
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms.fields import DateField
 
-from .models import Book, ImageBook, PersonReader,Author
+from . import models
+from .customwidjet import DateSelectorWidget
+from .models import Book, ImageBook, PersonReader, Author, Genre
 
 
 class BookForm(forms.ModelForm):
@@ -9,18 +14,36 @@ class BookForm(forms.ModelForm):
         fields = '__all__'
 
 
+class BookFormGenre(forms.ModelForm):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+    # name = forms.CharField(max_length=100, label='Жанр')
+
+
+class BookFormAuthors(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
 class ImageBookForm(forms.ModelForm):
     class Meta:
         model = ImageBook
         fields = '__all__'
 
+
 class PersonReaderForm(forms.ModelForm):
     class Meta:
         model = PersonReader
         fields = '__all__'
-        exclude = ['date_birthday']
+
+        widgets = {
+            'date_birthday': DateSelectorWidget,
+        }
+
 
 class Author_form(forms.ModelForm):
     class Meta:
-        model =Author
-        fields='__all__'
+        model = Author
+        fields = '__all__'
--- library/main/urls.py
+++ library/main/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 
-from .views import page_index, image_add, main_page,add_reader,add_author
+from .views import page_index, image_add, main_page, add_reader, add_author, book_genre_popup_add, book_author_popup_add, readers_page
 
 app_name = 'lib'
 
 urlpatterns = [
     path('register_book/', page_index, name='page_index'),
-    path('image_add/',image_add, name='image_add'),
+    path('image_add/', image_add, name='image_add'),
     path('', main_page, name='main_page'),
-    path('add_reader/',add_reader, name='add_reader'),
-    path('add_author', add_author, name='add_author')
+    path('add_reader/', add_reader, name='add_reader'),
+    path('add_author/', add_author, name='add_author'),
+    path('register_book1/', book_genre_popup_add, name='book_genre_popup_add'),
+    path('register_book2/', book_author_popup_add, name='book_author_popup_add'),
+    path('readers/', readers_page, name='readers_page')
 ]
